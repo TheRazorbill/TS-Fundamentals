80d135c67cd99fa6fe1a8df832abf36f1d87d764
Rahian S.S. <ter26621@gmail.com>
2026-01-10 11:57:38 -0300

    Refactor Contact and Todo interfaces; enhance type safety and improve function signatures

--- 0-linkedin_exercises/Ch03/03_02b/src/demo.ts
+++ 0-linkedin_exercises/Ch03/03_02b/src/demo.ts
@@ -2,7 +2,7 @@ type ContactName = string;
 type ContactStatus = "active" | "inactive" | "new"
 type ContactBirthDate = Date | number | string
 
-interface Contact  {
+interface Contact {
     id: number;
     name: ContactName;
     birthDate?: ContactBirthDate;
@@ -14,3 +14,12 @@ let primaryContact: Contact = {
     name: "Jamie Johnson",
     status: "active"
 }
+
+type ContactFields = keyof Contact
+const field : ContactFields = "status"
+
+function getValue <T, U extends keyof T>(source:T, propertyName: U){
+    return source[propertyName]
+}
+
+const value = getValue({min:1,max:34},"max")
